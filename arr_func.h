3df9132c39750702381752eb70a63d736f45d775
Baskov-Maxim <60775243+Baskov-Maxim@users.noreply.github.com>
2020-02-10 10:59:40 +0300

    Update arr_func.h

--- arr_func.h
+++ arr_func.h
@@ -1,9 +1,9 @@
 #ifndef ARR_FUNC_H		// Header Guards
 #define ARR_FUNC_H		// ARR_FUNC_H
 
-void set_arr(int* arr, const int SIZE);	     // Заполняем массив элементами
-void print_arr(int* arr, const int SIZE);	 // Вывод массива элементами
-void start_arr();                            // Функция старт-программы
+void set_arr(int* arr, const int SIZE);	      // Заполняем массив элементами
+void print_arr(int* arr, const int SIZE);	// Вывод массива элементами
+void start_arr();                             // Функция старт-программы
 bool check_mass(int* arr, const int SIZE);	// Проверка на уникальность
 void unique_arr(int* arr, const int SIZE);	// Заполнение уникальными элементами
 void max_min_arr(int* arr, const int SIZE);	// Поиск min и max значений
